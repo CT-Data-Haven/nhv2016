7a698f7db579564e26cfd830a262eac1c39c6ae1
camille-s <camille.seaberry@gmail.com>
2017-12-19 16:52:05 -0500

    Add download link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,7 @@ const text = 'Source: DataHaven analysis (2017) of US Census Bureau American Com
 const Footer = () => (
 	<div className="Footer">
 		<p>{text}</p>
+		<p><a href="https://github.com/camille-s/nhv2016/blob/master/public/data/2016_nhv_display.csv">Download all New Haven 2016 profile data</a></p>
 	</div>
 );
 
